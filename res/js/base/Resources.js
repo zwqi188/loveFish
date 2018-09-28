5becfba0f5e84fc4ccaf6ac8b28a197d2aae2f43
mac <mac@qizhengweideMacBook-Pro.local>
2018-09-28 22:15:01 +0800

    删除无用代码
    删除无用代码

--- res/js/base/Resources.js
+++
@@ -1,8 +0,0 @@
-export const Resources = [
-    ['background', 'res/background.png'],
-    ['land', 'res/land.png'],
-    ['pencilUp', 'res/pie_up.png'],
-    ['pencilDown', 'res/pie_down.png'],
-    ['birds', 'res/birds.png'],
-    ['startButton', 'res/start_button.png']
-];
